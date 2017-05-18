282588477a518f50317379785a6fad1099028134
Satanpit <alex.hyrenko@gmail.com>
2017-05-18 18:26:54 +0300

    Add scroll on click on menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,5 @@
 import Promise from 'promise-polyfill';
+import SmoothScroll from 'smoothscroll-polyfill';
 
 import { $ } from './dom';
 import { fetchJSON } from './utils';
@@ -12,10 +13,25 @@ if (!window.Promise) {
   window.Promise = Promise;
 }
 
+SmoothScroll.polyfill();
+
 $('.tabs').forEach(node => new Tabs(node));
 $('.slider').forEach(node => new Slider(node));
 
 fetchJSON('data/stats.json').then(data => {
   $('.map').forEach(node => new Map(node, data.slice()));
   $('.declarations').forEach(node => new Statistic(node, data));
+});
+
+document.querySelector('.header__nav').addEventListener('click', e => {
+  e.preventDefault();
+
+  if (e.target.href === undefined) {
+    return;
+  }
+
+  const to = e.target.href.split('#')[1];
+  const { top } = document.getElementById(to).getBoundingClientRect();
+
+  window.scroll({ top: (top + window.scrollY) - 50, left: 0, behavior: 'smooth' });
 });
