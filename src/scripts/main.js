d98986000c0f26b9f5f4624ed85d8ca6269d2cfc
Alex <alex.hyrenko@gmail.com>
2017-06-19 11:53:58 +0300

    Crazy feedback (#10)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,6 +9,7 @@ import Tabs from './tabs';
 import Slider from './slider';
 import Map from './map';
 import Statistic from './statistic';
+import Feedback from './feedback';
 
 if (!window.Promise) {
   window.Promise = Promise;
@@ -19,6 +20,7 @@ SmoothScroll.polyfill();
 $('.nav').forEach(node => new Nav(node));
 $('.tabs').forEach(node => new Tabs(node));
 $('.slider').forEach(node => new Slider(node));
+$('.feedback').forEach(node => new Feedback(node));
 
 fetchJSON('data/stats.json').then(data => {
   $('.map').forEach(node => new Map(node, data.slice()));
