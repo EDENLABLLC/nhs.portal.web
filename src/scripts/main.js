22063677abc8e084d0d32f6094e1726de26cb507
ivchenkodima <ivchenkodima@gmail.com>
2017-08-08 14:32:56 +0300

    integrate stat and map with real data

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,6 +23,16 @@ $('.slider').forEach(node => new Slider(node));
 $('.feedback').forEach(node => new Feedback(node));
 
 fetchJSON('data/stats.json').then(data => {
-  $('.map').forEach(node => new Map(node, data.slice()));
   $('.declarations').forEach(node => new Statistic(node, data));
+});
+
+fetchJSON('http://dev.ehealth.world/reports/stats/regions').then(data => {
+  $('.map').forEach(node => new Map(node, data.data));
+});
+
+fetchJSON('http://dev.ehealth.world/reports/stats/').then(data => {
+  const joined_items = $('.joined__item-count');
+  joined_items[0].innerText = data.data.msps;
+  joined_items[1].innerText = data.data.doctors;
+  joined_items[2].innerText = data.data.declarations;
 });
