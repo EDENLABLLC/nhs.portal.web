e6c8427a09f5755d4feeead0d58b85b7bc850c57
ivchenkodima <ivchenkodima@gmail.com>
2017-08-11 18:10:57 +0300

    add statistic barcharts

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -29,7 +29,38 @@ fetchJSON('data/stats.json').then(data => {
   // $('.declarations').forEach(node => new Statistic(node, data));
 });
 
-const ctx = document.getElementById('declarations__graph-canvas').getContext('2d');
+const REGION__MAP = {
+  "Київ": 502,
+  "Луцьк": 523,
+  "Львов": 423,
+  "Ужгород": 324,
+  "Ивано-Франковск": 232,
+  "Черновцы": 252,
+  "Тернополь": 232,
+  "Ровно": 200,
+  "Хмельницкий": 182,
+  "Житомир": 132,
+  "Винница": 92,
+  "Черкассы": 82,
+  "Кировоград": 66,
+  "Полтава": 50,
+  "Чернигов": 32,
+  "Суммы": 12,
+  "Харьков": 9,
+  "Луганск": 8,
+  "Днепропетровск": 7,
+  "Донецк": 6,
+  "Запорожье": 5,
+  "Херсон": 4,
+  "Николаев": 3,
+  "Одесса": 2,
+  "Крым": 1,
+};
+
+// Dinamical number of declaraion, mis, doctors
+const dinamical_declaration = document.getElementById('declarations__graph-canvas').getContext('2d');
+// const dinamical_doctors = document.getElementById('declarations__graph-canvas').getContext('2d');
+// const dinamical_mis = document.getElementById('declarations__graph-canvas').getContext('2d');
 
 const DATA = {
   labels: ["January", "February", "March", "April", "May", "June", "July"],
@@ -102,13 +133,31 @@ const customTooltips = function(tooltip) {
   tooltipEl.style.padding = tooltip.yPadding + 'px ' + tooltip.xPadding + 'px';
 };
 
-const myLineChart = new Chart(ctx, {
+const dinamicalDeclarationChart = new Chart(dinamical_declaration, {
   type: 'line',
   data: DATA,
   options: {
-    title:{
+    title: {
       display: true,
-      text: 'Chart.js Line Chart - Custom Tooltips'
+      text: 'Chart.js Line Chart - Custom Tooltips',
+    },
+    scales: {
+      xAxes: [{
+        gridLines: {
+          drawBorder: false,
+          drawTicks: true,
+          zeroLineWidth: 1,
+          zeroLineColor: "rgba(232,232,232,1)",
+        },
+      }],
+      yAxes: [{
+        gridLines: {
+          drawBorder: false,
+          drawTicks: true,
+          zeroLineWidth: 1,
+          zeroLineColor: "rgba(232,232,232,1)",
+        },
+      }]
     },
     tooltips: {
       position: 'nearest',
@@ -127,3 +176,99 @@ const myLineChart = new Chart(ctx, {
   }
 });
 
+
+// Number of declarations, mis, doctors
+const NUMBER_BY_REGION_DECLARATION = document.getElementById('declarations_number__graph-canvas').getContext('2d');
+
+const REGION_DECLARATIONS = {
+  labels: Object.keys(REGION__MAP).map( i => i),
+  datasets: [{
+    backgroundColor: 'rgb(98, 164, 240)',
+    borderColor: 'rgb(72,98,237)',
+    data: Object.values(REGION__MAP).map( i => i),
+  }],
+};
+
+
+const NUMBER_BY_REGION = new Chart(NUMBER_BY_REGION_DECLARATION, {
+  type: 'horizontalBar',
+  data: REGION_DECLARATIONS,
+  onAnimationComplete: function() {
+    console.log(this.chart);
+    let ctx = this.chart.ctx;
+    ctx.font = this.scale.font;
+    ctx.fillStyle = this.scale.textColor;
+    ctx.textAlign = "center";
+    ctx.textBaseline = "bottom";
+
+    this.datasets.forEach(function(dataset)  {
+      dataset.bars.forEach(function(bar) {
+        ctx.fillText(bar.value, bar.x, bar.y - 5);
+      });
+    })
+  },
+  scaleLineColor: "rgba(0,0,0,0)",
+  options: {
+    title: {
+      display: false,
+    },
+    legend: {
+      display: false,
+      labels: {
+        padding: 20,
+      }
+    },
+    tooltipTemplate: "<%= value %>",
+    scaleLineColor: "rgba(0,0,0,0)",
+    scales: {
+      xAxes: [{
+        gridLines: {
+          lineWidth: 0,
+          display: false,
+          drawBorder: false,
+          drawTicks: false,
+        },
+        ticks: {
+          display: false,
+        }
+      }],
+      yAxes: [{
+        barPercentage: 0.95,
+        categoryPercentage: 1,
+        ticks: {
+          padding: 25,
+        },
+        gridLines: {
+          display: false,
+          drawBorder: false,
+          drawTicks: false,
+        },
+      }]
+    },
+    tooltips: {
+      enabled: false
+    },
+    hover: {
+      animationDuration: 0
+    },
+    animation: {
+      duration: 1,
+      onComplete: function () {
+        let chartInstance = this.chart,
+          ctx = chartInstance.ctx;
+        ctx.font = Chart.helpers.fontString(14, '700', 'GothamPro');
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'bottom';
+        ctx.fillStyle = '#000';
+
+        this.data.datasets.forEach(function (dataset, i) {
+          let meta = chartInstance.controller.getDatasetMeta(i);
+          meta.data.forEach(function (bar, index) {
+            let data = dataset.data[index];
+            ctx.fillText(data, bar._model.x + 20, bar._model.y + 7);
+          });
+        });
+      }
+    }
+  }
+});
