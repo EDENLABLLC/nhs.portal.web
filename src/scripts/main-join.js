dfddbe1f34a93b7566538520407e89f4e546993e
Alexey Bondarenko <alexeybondarenko@me.com>
2017-08-28 00:26:27 +0300

    Add MIS pages

--- src/scripts/main-join.js
+++ src/scripts/main-join.js
@@ -1,10 +1,19 @@
 import { $ } from './dom';
 import Tabs from './tabs';
 import Back from './back';
+import ToggleClass from './toggleClass';
 
 $('.tabs').forEach(node => {
   const tab = new Tabs(node);
   tab.selectElemByHash(location.hash.slice(1));
   return tab;
 });
-$('.js-back-link').forEach(node => new Back(node));
+const $togglers = $('.providers-list__requirements');
+
+$togglers.forEach(node => {
+  node.addEventListener('click', () => {
+    $togglers.filter(a => node !== a).forEach(node => node.classList.remove('providers-list__requirements_active'));
+  })
+  new ToggleClass(node, 'providers-list__requirements_active')
+});
+// $('.js-back-link').forEach(node => new Back(node));
