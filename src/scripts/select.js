b890fb3d175d4bd1ebf3324f9a350ba1a7d85423
ivchenkodima <ivchenkodima@gmail.com>
2017-09-14 16:37:06 +0300

    add icons, fix select

--- src/scripts/select.js
+++ src/scripts/select.js
@@ -2,17 +2,11 @@ import Choices from 'choices.js';
 import { $ } from './dom';
 
 const select_elem = $('#select')[0];
-// console.log(select_elem);
 
 const example = new Choices(select_elem, {
-  placeholderValue: "Оберіть тему",
   searchPlaceholderValue: "Ввідіть тему",
   noResultsText: "Не знайдено",
-  itemSelectText: ''
-});
+  itemSelectText: '',
+  searchEnabled: false,
 
-// example.setChoices([
-//   {value: 'One', label: 'Label One', disabled: true},
-//   {value: 'Two', label: 'Label Two', selected: true},
-//   {value: 'Three', label: 'Label Three'},
-// ], 'value', 'label', false);
+});
