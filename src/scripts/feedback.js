4826c55d9adf298b04a08600a5b8ac07c4e87238
ivchenkodima <ivchenkodima@gmail.com>
2017-09-18 17:44:18 +0300

    final fixes

--- src/scripts/feedback.js
+++ src/scripts/feedback.js
@@ -7,11 +7,13 @@ export default class Feedback extends BEM {
     this.$name = this.elem('name');
     this.$message = this.elem('message');
     this.$mailTo = this.elem('mailto');
+    this.$theme = this.elem('theme');
+    console.log('this.$theme', this.$theme);
 
     node.onsubmit = (e) => {
       e.preventDefault();
 
-      this.$mailTo.href = `mailto:info@nszu.gov.ua?subject=Зворотній зв’язок від ${this.$name.value}&body=${this.$message.value}`;
+      this.$mailTo.href = `mailto:info@ehealth-ukraine.org?subject=Зворотній зв’язок: ${this.$theme.value} ${this.$name.value}&body=${this.$message.value}`;
       this.$mailTo.click();
       return false;
     }
