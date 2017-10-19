a2fa2175ecfca960eac7cf4dd2e94deb4c8d5b62
Dmitry Ivchenko <dima@nebo15.com>
2017-10-19 13:19:30 +0300

    Reimbursement statistics. Connects to edenlabllc/ehealth.api#1161 (#31)
    * New stat reports
    
    * stats by region
    
    * add line chart statistics

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -44,26 +44,47 @@ const year = new Date().getFullYear();
 fetchJSON(`${API_ENDPOINT}/reports/stats/histogram?from_date=${year}-${month}-01&to_date=${year}-${month}-${day}&interval=DAY`)
   .then(data => {
     const MONTH_REGION_DECLARATION = document.getElementById('declarations__graph-canvas').getContext('2d');
-    const DATA = data.data.reduce((acc, cur, index) => {
+    const MONTH_REGION_MEDICATION = document.getElementById('medication_requests__graph-canvas').getContext('2d');
+
+    const DATA_DECLARATION = data.data.reduce((acc, cur, index) => {
       acc.push({
         value: cur.declarations_active_end,
         label: index + 1,
         ...cur.stats,
       });
-      return acc
+      return acc;
+    },[]);
+
+    const DATA_MEDICATION = data.data.reduce((acc, cur, index) => {
+      acc.push({
+        value: cur.medication_requests_active_end,
+        label: index + 1,
+        ...cur.stats,
+      });
+      return acc;
     },[]);
     DinamicalMonthChart(
       MONTH_REGION_DECLARATION,
-      DATA.map(i => i.label),
-      DATA.map(i => i.value),
+      DATA_DECLARATION.map(i => i.label),
+      DATA_DECLARATION.map(i => i.value),
+      'declarations__graph-canvas'
+    );
+    DinamicalMonthChart(
+      MONTH_REGION_MEDICATION,
+      DATA_MEDICATION.map(i => i.label),
+      DATA_MEDICATION.map(i => i.value),
+      'medication_requests__graph-canvas'
     );
   });
 
 fetchJSON(`${API_ENDPOINT}/reports/stats/regions`).then(data => {
   $('.map').forEach(node => new Map(node, data.data));
-  const NUMBER_BY_REGION_DECLARATION = document.getElementById('declarations_number__graph-canvas').getContext('2d');
   const NUMBER_BY_REGION_MSPS = document.getElementById('msp_number__graph-canvas').getContext('2d');
-  const NUMBER_BY_REGION_DOCTORS= document.getElementById('doctors_number__graph-canvas').getContext('2d');
+  const NUMBER_BY_REGION_FARMACIES = document.getElementById('pharmacies_number__graph-canvas').getContext('2d');
+  const NUMBER_BY_REGION_DOCTORS = document.getElementById('doctors_number__graph-canvas').getContext('2d');
+  const NUMBER_BY_REGION_FARMACIST = document.getElementById('pharmacists_requests_number__graph-canvas').getContext('2d');
+  const NUMBER_BY_REGION_REQUEST = document.getElementById('medication_requests_number__graph-canvas').getContext('2d');
+  const NUMBER_BY_REGION_DECLARATION = document.getElementById('declarations_number__graph-canvas').getContext('2d');
 
   let DATA = data.data.reduce((acc, cur ) => {
     acc.push({
@@ -74,25 +95,43 @@ fetchJSON(`${API_ENDPOINT}/reports/stats/regions`).then(data => {
   }, []);
 
   RegionsCharts(
-    NUMBER_BY_REGION_DECLARATION,
-    DATA.sort((a, b) => a.declarations - b.declarations).reverse(),
-    'declarations'
-  );
-  RegionsCharts(
     NUMBER_BY_REGION_MSPS,
     DATA.sort((a, b) => a.msps - b.msps).reverse(),
     'msps',
+  );
+  RegionsCharts(
+    NUMBER_BY_REGION_FARMACIES,
+    DATA.sort((a, b) => a.pharmacies - b.pharmacies).reverse(),
+    'pharmacies',
   );
   RegionsCharts(
     NUMBER_BY_REGION_DOCTORS,
     DATA.sort((a, b) => a.doctors - b.doctors).reverse(),
     'doctors',
   );
+  RegionsCharts(
+    NUMBER_BY_REGION_FARMACIST,
+    DATA.sort((a, b) => a.pharmacists - b.pharmacists).reverse(),
+    'pharmacists',
+  );
+  RegionsCharts(
+    NUMBER_BY_REGION_REQUEST,
+    DATA.sort((a, b) => a.medication_requests - b.medication_requests).reverse(),
+    'medication_requests',
+  );
+  RegionsCharts(
+    NUMBER_BY_REGION_DECLARATION,
+    DATA.sort((a, b) => a.declarations - b.declarations).reverse(),
+    'declarations'
+  );
 });
 
 fetchJSON(`${API_ENDPOINT}/reports/stats/`).then(data => {
   const joined_items = $('.joined__item-count');
+
   joined_items[0].innerText = data.data.msps;
-  joined_items[1].innerText = data.data.doctors;
-  joined_items[2].innerText = data.data.declarations;
+  joined_items[1].innerText = data.data.pharmacies;
+  joined_items[2].innerText = data.data.doctors;
+  joined_items[3].innerText = data.data.pharmacists;
+  joined_items[4].innerText = data.data.declarations;
 });
