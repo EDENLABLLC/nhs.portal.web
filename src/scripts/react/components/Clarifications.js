ff0617fcf51c755665781fb46e8d3bfe66192ac1
Myroslav Kiurchev <kiurchv@gmail.com>
2017-11-06 11:15:32 +0200

    Detach tabs into separate component

--- src/scripts/react/components/Clarifications.js
+++ src/scripts/react/components/Clarifications.js
@@ -1,107 +1,62 @@
 import React, { Component } from "react";
 import classnames from "classnames";
 import { format } from "date-fns";
-import Carousel from "nuka-carousel";
 
-class Clarifications extends Component {
-  state = {
-    activeTypeIndex: 0
-  };
+import TabView from "./TabView";
 
-  tabs = {};
+const Clarifications = ({ clarifications, types, limit }) => (
+  <section className="clarifications">
+    <header>
+      <h3>Пояснення</h3>
+    </header>
 
-  render() {
-    const { clarifications, types, limit } = this.props;
-    const { activeTypeIndex } = this.state;
-
-    return (
-      <section className="clarifications">
-        <header>
-          <h3>Пояснення</h3>
-        </header>
-        <article>
-          <header className="tabs__header">
-            <ul className="tabs__nav">
-              {types.map((type, index) => (
-                <li
-                  key={type}
-                  ref={e => (this.tabs[index] = e)}
-                  className={classnames("tabs__nav-item", {
-                    "tabs__nav-item_active": activeTypeIndex === index,
-                    "tabs__nav-item_marker": activeTypeIndex === index
-                  })}
-                  onClick={() => this.changeType(index, true)}
-                >
-                  {type}
-                </li>
+    <TabView>
+      {types.map(type => ({
+        title: type,
+        content: (
+          <div key={type} className="clarifications__list">
+            {clarifications
+              .filter(
+                ({ clarificationTypes }) =>
+                  type === "Всім" ||
+                  (clarificationTypes && clarificationTypes.includes(type))
+              )
+              .slice(0, limit)
+              .map(({ id, url, title, excerpt, date }) => (
+                <a key={id} className="clarifications__item" href={url}>
+                  <h4>{title}</h4>
+                  <time>{format(date, "DD.MM.YYYY")}</time>
+                  {excerpt}
+                </a>
               ))}
-            </ul>
-          </header>
-          <Carousel
-            ref={e => (this.carousel = e)}
-            className="tabs__main"
-            afterSlide={this.changeType}
-            decorators={null}
-            cellSpacing={20}
-            swiping
-          >
-            {types.map(type => (
-              <div key={type} className="clarifications__list">
-                {clarifications
-                  .filter(
-                    ({ clarificationTypes }) =>
-                      type === "Всім" ||
-                      (clarificationTypes && clarificationTypes.includes(type))
-                  )
-                  .slice(0, limit)
-                  .map(({ id, url, title, excerpt, date }) => (
-                    <a key={id} className="clarifications__item" href={url}>
-                      <h4>{title}</h4>
-                      <time>{format(date, "DD.MM.YYYY")}</time>
-                      {excerpt}
-                    </a>
-                  ))}
-              </div>
-            ))}
-          </Carousel>
-        </article>
-        {/* <section className="explain__tags">
-              <div className="explain__tags-title">
-                  Теги:
-              </div>
-              <ul className="explain__tags-list">
-                  <li className="explain__tags-item">
-                      <a className="explain__tags-link" href="#">ЕЦП</a>
-                  </li>
-                  <li className="explain__tags-item">
-                      <a className="explain__tags-link" href="#">НСЗУ</a>
-                  </li>
-                  <li className="explain__tags-item">
-                      <a className="explain__tags-link" href="#">Пояснення</a>
-                  </li>
-              </ul>
-          </section> */}
-        {limit && (
-          <footer className="explain__footer">
-            <a href="clarifications.html">Показати більше</a>
-          </footer>
-        )}
-      </section>
-    );
-  }
+          </div>
+        )
+      }))}
+    </TabView>
 
-  changeType = (index, updateCarousel) => {
-    const tab = this.tabs[index];
-    const { types } = this.props;
+    {/* <section className="explain__tags">
+      <div className="explain__tags-title">
+          Теги:
+      </div>
+      <ul className="explain__tags-list">
+        <li className="explain__tags-item">
+          <a className="explain__tags-link" href="#">ЕЦП</a>
+        </li>
+        <li className="explain__tags-item">
+          <a className="explain__tags-link" href="#">НСЗУ</a>
+        </li>
+        <li className="explain__tags-item">
+          <a className="explain__tags-link" href="#">Пояснення</a>
+        </li>
+      </ul>
+    </section> */}
 
-    this.setState({ activeTypeIndex: index });
-    tab.scrollIntoView({
-      behavior: "smooth",
-      block: "nearest",
-      inline: "nearest"
-    });
-    if (updateCarousel) this.carousel.goToSlide(index);
-  };
-}
+    {limit && (
+      <footer className="explain__footer">
+        <a href="clarifications.html">Показати більше</a>
+      </footer>
+    )}
+  </section>
+);
 
 export default Clarifications;
