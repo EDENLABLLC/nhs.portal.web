20b916c9c083eb23293fc6f45b8027fc008f51bb
ivchenkodima <ivchenkodima@gmail.com>
2017-11-07 00:58:01 +0200

    remove data

--- src/scripts/react/components/Statistics.js
+++ src/scripts/react/components/Statistics.js
@@ -1,24 +1,5 @@
 import React from 'react';
-import classnames from 'classnames';
 import TabView from "./TabView";
-
-const data = [{
-  title: "Динаміка реєстрації декларацій",
-  content: '<canvas class="declarations__graph" id="declarations__graph-canvas" width="700" height="350"></canvas>'
-}, {
-  title: "Кількість закладів, лікарів, декларацій<br />  по регіонах",
-  content: [{
-    title: "Заклади",
-    content: '<canvas class="declarations__graph" id="msp_number__graph-canvas" width="700" height="350"></canvas>'
-  },{
-    title: "Лікарі",
-    content: '<canvas class="declarations__graph" id="doctors_number__graph-canvas" width="700" height="350"></canvas>'
-  }, {
-    title: "Декларації",
-    content: '<canvas class="declarations__graph" id="declarations_number__graph-canvas" width="700" height="350"></canvas>'
-  }]
-}
-];
 
 export default class Statistics extends React.Component {
   render() {
